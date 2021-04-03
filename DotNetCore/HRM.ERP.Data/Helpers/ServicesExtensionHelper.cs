293e5c2e3dc5d38a4fb644a3b3f5b4a07abf01e5
Dhruv Suthar <intelligentjimmy@gmail.com>
2021-04-03 13:53:09 +0530

    fixes

---
+++ DotNetCore/HRM.ERP.Data/Helpers/ServicesExtensionHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HRM.ERP.Data.Helpers
+{
+    public static class ServicesExtensionHelper
+    {
+        public static IServiceCollection RegisterDataServices(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(d =>
+                d.UseSqlite(configuration.GetConnectionString("ERPDatabase")));
+            // options.UseSqlite(Configuration.GetConnectionString("WebApiDatabase"));
+            services.AddAutoMapper(typeof(ServicesExtensionHelper));
+
+            return services;
+        }
+    }
+}
